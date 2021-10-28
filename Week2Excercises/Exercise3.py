7916af9136fa6945b3f1a74a4eeb8a10ee6b1d67
DMacewan2522 <2005884@brunel.ac.uk>
2021-10-28 05:52:48 +0100

    Exercise 3 - Password Guesser

---
+++ Week2Excercises/Exercise3.py
@@ -0,0 +1,14 @@
+passWord = "Secret"
+
+counter = 3
+
+while counter > 0:
+    userGuess = input("Attempt to guess my secret password: ")
+    if userGuess == passWord:
+        print("Well done, you guessed the password!")
+        break
+    elif userGuess != passWord and counter > 1:
+        print("Try Again")
+        counter -= 1
+    else:
+        print("You Lose")
