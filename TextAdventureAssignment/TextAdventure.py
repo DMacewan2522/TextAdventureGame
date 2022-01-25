9804eb0528984f643016c6c8e5a8c655d59ea15d
DMacewan2522 <2005884@brunel.ac.uk>
2022-01-25 00:51:55 +0000

    Commit TextAdventure #3
    Updates:
     -Updated comments throughout code
     -Extended starting room actions
     -Created sequence that follows from the player leaving the starting room

--- TextAdventureAssignment/TextAdventure.py
+++ TextAdventureAssignment/TextAdventure.py
@@ -1,4 +1,3 @@
-#Looking around lets them find a small sword that a goblin left behind and begin escaping.
 #They face some goblins and a goblin boss in the cave as well as find some items.
 #Goblin chief fight
 #When they leave the cave they find a forest with a shrine at the end of it.
@@ -8,23 +7,27 @@
 
 ###################################################
 
+#Module Imports
 import time
+import random
 import TextAdventureDescriptionText as discText
 
+#Player stats dictionary
 playerDictionary = {"Inventory": [], "Health": 100}
 
-playerAlive = True
-
+#Global Variables
 restraintsEscaped = False
-storageInputList = ["barrel", "barrels", "crate", "crates", "storage"]
 
+#Function that allows text to be printed onto the screen character by character (Essentially replaces print)
 def typeText(text):
     for ch in text:
         print(ch, end="")
         time.sleep(0.005)
 
+#Intro sequence
 def StartSequence():
     time.sleep(1)
+    #Game Title
     typeText("""
   ______   ______    __   __       __  .__   __.   _______      ______     ___   ____    ____  _______     _______.
  /      | /  __  \  |  | |  |     |  | |  \ |  |  /  _____|    /      |   /   \  \   \  /   / |   ____|   /       |
@@ -34,10 +37,11 @@ def StartSequence():
  \______| \______/  |__| |_______||__| |__| \__|  \______|     \______/__/     \__\  \__/     |_______|_______/    
                                                                                                                    """)
     time.sleep(3)
+    #Dialogue to introduce the character to their enemies
     typeText("\n\nLocation: ???")
     time.sleep(2)
     typeText("\n\nYou wake up, dazed and confused with hands and feet tied.\n"
-          "As you start to wake up you hear the grunts of goblins around you and stiffen into alertness.")
+             "As you start to wake up you hear the grunts of goblins around you and stiffen into alertness.")
     time.sleep(3)
     typeText("\n\nGoblin 1:")
     typeText("\nWell look who finally woke up... Dinner!")
@@ -54,26 +58,25 @@ def StartSequence():
     typeText("\n\nThe three goblins leave the damp dingy cave you are captive in and slam the door shut.")
     UserInputStartingActions()
 
+#Called upon when the player inputs to escape their restraints
 def RestraintEscape():
     time.sleep(2)
+    # Calls upon another .py file that stores large descriptive text. This is used throughout the code
     typeText(discText.restraintEscapeText)
     restraintsEscaped = True
     UserInputRoomActions()
 
+#Called upon when the player inputs to look around the room they wake up in
 def StartRoomInspect():
     time.sleep(2)
     typeText(discText.startRoomLookText)
+    #If statement to ensure the player doesn't become restrained again if they have already performed that action
     if restraintsEscaped == True:
         UserInputRoomActions()
     else:
         UserInputStartingActions()
 
-def StorageInspect():
-    time.sleep(2)
-    typeText(discText.storageInspectText)
-    UserInputRoomActions()
-
-
+#The input options that the player has when they are restrained in the first room
 def UserInputStartingActions():
     userInputStarting = input("\n\nWhat would you like to do? ")
     if "restraints" in userInputStarting.lower():
@@ -84,6 +87,7 @@ def UserInputStartingActions():
         print("Invalid input")
         UserInputStartingActions()
 
+#The input options that the player has when they have escaped their restraints
 def UserInputRoomActions():
     userInputRoom = input("\n\nWhat would you like to do? ")
     if "take" and "sword" in userInputRoom.lower():
@@ -96,6 +100,21 @@ def UserInputRoomActions():
         print("Invalid input")
         UserInputRoomActions()
 
+#Called upon when the player chooses to leave the starting room
 def CaveCorridor():
+    #Random enemy generator. Essentially flips a coin to see if an enemy is present or not
+    enemyCaveCorridorPresent = random.randint(1, 2)
+    time.sleep(2)
+    typeText(discText.caveCorridorDescription)
+    time.sleep(2)
+    #Begins combat sequence with the enemy if they are present
+    if enemyCaveCorridorPresent == 1:
+        typeText("\nWithout having time to think, a goblin comes walking towards you "
+                 "from the direction you need to go... He draws his sword and charges!")
+    #Asks the player what they wish to do if there is no enemy present
+    else:
+        userInputCaveCorridor = input("\nWhat would you like to do? ")
 
+
+#Begins the game
 StartSequence()
--- TextAdventureAssignment/TextAdventureDescriptionText.py
+++ TextAdventureAssignment/TextAdventureDescriptionText.py
@@ -2,9 +2,10 @@ restraintEscapeText = "\n\nYou struggle and struggle until finally you slip out 
                       "untying the rope around your feet with your newly freed hands"
 
 startRoomLookText = "\n\nYou are in a damp cave room with torches dimly lighting the room.\n" \
-                    "Within the room are multiple small storage boxes and barrels and a table with an apple\n"\
-                    "There is also a short sword resting on the wall behind one of the storage boxes...\n" \
+                    "Within the room are multiple small boxes and barrels and a table with a candle lit upon it\n"\
+                    "There is also a short sword resting on the wall behind one of the boxes...\n" \
                     "Probably left by a goblin"
 
-storageInspectText = "\n\nYou find nothing looking through the storage in the room"
+caveCorridorDescription = "\n\nYou leave the room you were held captive in, looking out into a large cave" \
+                          "The walls are lined with torches leading you through the corridor"
 
